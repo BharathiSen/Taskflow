e2bcd2730fbc8acbf65b9304b356213d9b4e9f59
Bharathis28 <bharathis.ece2023@citchennai.net>
2025-12-18 09:27:27 +0530

    add test_connection function to validate database connectivity

--- backend/app/database.py
+++ backend/app/database.py
@@ -5,3 +5,12 @@ DATABASE_URL = "postgresql://postgres:bharathirsa@28@localhost/taskflow_db" # Da
 
 engine = create_engine(DATABASE_URL) # Create the database engine
 SessionLocal = sessionmaker(bind=engine) # One Database session per request 
+
+def test_connection(): 
+    
+    db=SessionLocal() # Create a new database session
+    try:
+        db.execute("SELECT 1") # Simple query to test connection
+        print("Database connection successful")
+    finally:
+        db.close()
