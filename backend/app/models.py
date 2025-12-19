c175692a3856db3cf0bbf841c78d90dfac8340cc
Bharathis28 <bharathis.ece2023@citchennai.net>
2025-12-19 09:59:48 +0530

    fix: update created_at column default value in Task model

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,7 +5,7 @@ from sqlalchemy import Integer,String,ForeignKey,DateTime # Data types for model
 from sqlalchemy.orm import Mapped, mapped_column # For defining mapped columns
 from datetime import datetime # For handling date and time  
 class Base(DeclarativeBase):
-    
+    pass
     
 # Organization model representing organizations in the database
 class Organization(Base):
@@ -33,6 +33,5 @@ class Task(Base):  # Task model representing tasks in the database
     id: Mapped[int]=mapped_column(Integer, primary_key=True) # Primary key column
     title: Mapped[str]=mapped_column(String, nullable=False) # Title column
     status: Mapped[str]=mapped_column(String, nullable=False) # Status column
-    created_at: Mapped[datetime]=mapped_column(DateTime, default=datetime.utcnow) # Created at column with default value
-
+    created_at: Mapped[datetime]=mapped_column(DateTime, default=datetime) # Created at column with default value
     organization_id: Mapped[int]=mapped_column(ForeignKey("organizations.id"), nullable=False) # Foreign key column referencing organizations table
