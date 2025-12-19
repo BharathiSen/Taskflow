dec071e71cc5917f6dc28a000dc1fd750baccbb1
Bharathis28 <bharathis.ece2023@citchennai.net>
2025-12-19 08:37:25 +0530

    fix: correct database URL and improve connection test query

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,16 +1,17 @@
+from sqlalchemy import text # For executing raw SQL queries
 from sqlalchemy import create_engine # Creates connection to the database
 from sqlalchemy.orm import sessionmaker # Manages database sessions
 
-DATABASE_URL = "postgresql://postgres:bharathirsa@28@localhost/taskflow_db" # Database connection URL
+DATABASE_URL = "postgresql://postgres:bharathi@localhost:5432/taskflow_db" # Database connection URL
 
 engine = create_engine(DATABASE_URL) # Create the database engine
 SessionLocal = sessionmaker(bind=engine) # One Database session per request 
 
 def test_connection(): 
-    
+
     db=SessionLocal() # Create a new database session
     try:
-        db.execute("SELECT 1") # Simple query to test connection
+        db.execute(text("SELECT 1")) # Simple query to test connection
         print("Database connection successful")
     finally:
         db.close()
